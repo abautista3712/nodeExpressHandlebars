80bc1055ee3a9dc162995648b654aaa2dbe9a3fe
Abelard Bautista <abelardbautista@gmail.com>
2020-04-04 18:58:20 -0700

    Initialized working environment.

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Animal Style Cheeseburger', TRUE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Cheeseburger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Hamburger', FALSE);
