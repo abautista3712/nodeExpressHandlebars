23d7f38dceb835c2b73eaf76e3047d2150e10ea9
Abelard Bautista <abelardbautista@gmail.com>
2020-04-14 17:36:54 -0700

    HTML form POSTs to console.

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -16,8 +16,8 @@ router.get("/", function (req, res) {
 
 router.post("/api/burgers", function (req, res) {
   burger.create(
-    ["name", "devoured"],
-    [req.body.name, req.body.devoured],
+    ["burger_name", "devoured"],
+    [req.body.burger_name, req.body.devoured],
     function (result) {
       res.json({ id: result.insertId });
     }
